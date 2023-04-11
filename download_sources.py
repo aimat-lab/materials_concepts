24682e7e56f90e9ec5387a80112a1c7a5e5fc609
thomasmarwitz <thomasmarwitz3@gmail.com>
2023-04-11 22:59:59 +0200

    Impl new handler and converter system

--- download_sources.py
+++ download_sources.py
@@ -1,24 +1,30 @@
-from Downloader import OADownloader
+from Downloader import OADownloader, InMemoryHandler, Converter
 from utils.utils import make_valid_filename
 
-TOPIC = "material science"
+TOPIC = "materials science"
 SOURCES_URL = f"https://api.openalex.org/sources?search={TOPIC}"
 
 downloader = OADownloader(
     url=SOURCES_URL,
     fields=["id", "display_name", "works_count", "type", "alternate_titles"],
     per_page=200,  # 200 is the maximum
+    handler=InMemoryHandler(),
 )
+
 
 cleaned_topic = make_valid_filename(TOPIC)
 filename = f"{cleaned_topic}_sources.csv"
 
-downloader.get().to_csv(
-    filename=filename,
-    converters={
+converter = Converter(
+    {
         "alternate_titles": lambda x: x[0]
         if x
         else "",  # keep first if there is a non-empty list
         "id": lambda x: x.rsplit("/", 1)[1],  # remove the URL prefix
-    },
+    }
+)
+
+downloader.get().to_csv(
+    filename=filename,
+    converter=converter,
 )
