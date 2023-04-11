d8fea3e009b61f8745116ad71d9bfff217ebb256
thomasmarwitz <thomasmarwitz3@gmail.com>
2023-04-11 18:48:52 +0200

    Rewrite script to accept sources.csv file.
    All host_venue_ids in column 'id' are processed and written to dedicated directory
    inside of 'data/'.

--- download_works.py
+++ download_works.py
@@ -1,49 +1,74 @@
 # custom imports
-from Downloader import OADownloader
-from utils.utils import inverted_abstract_to_abstract, extract_concepts, Timer
+from Downloader import OADownloader, InMemoryHandler, FileHandler, Converter
+import utils.utils as utils
 
 WORKS_URL = "https://api.openalex.org/works"
 
+FIELDS = [
+    "id",
+    "doi",
+    "display_name",
+    "publication_date",
+    "is_retracted",
+    "is_paratext",
+    "abstract_inverted_index",
+    "concepts",
+]
 
-def main(host_venue, fetch_limit, output):
+
+def fetch_single(host_venue, fetch_limit, output):
+    converter = Converter(
+        {
+            "id": lambda x: x.rsplit("/", 1)[1],
+            "abstract_inverted_index": utils.inverted_abstract_to_abstract,
+            "concepts": utils.extract_concepts,
+        }
+    )
+
+    handler = FileHandler(
+        output, fields=FIELDS, converter=converter, flush_threshold=5000
+    )
+
     downloader = OADownloader(
         url=WORKS_URL,
-        fields=[
-            "id",
-            "doi",
-            "display_name",
-            "publication_date",
-            "is_retracted",
-            "is_paratext",
-            "abstract_inverted_index",
-            "concepts",
-        ],
+        fields=FIELDS,
         per_page=200,  # 200 is max
-        fetch_limit=fetch_limit,  # limit is 10.000, implement cursor fetching for more
+        handler=handler,
+        fetch_limit=fetch_limit,
         filter=f"host_venue.id:{host_venue}",
     )
 
-    with Timer("Downloading works"):
-        df = downloader.get().to_df(
-            converters={
-                "id": lambda x: x.rsplit("/", 1)[1],
-                "abstract_inverted_index": inverted_abstract_to_abstract,
-                "concepts": extract_concepts,
-            }
-        )
+    with utils.Timer("Downloading works"):
+        downloader.get()  # file handler stores data
 
-    df = df.rename(columns={"abstract_inverted_index": "abstract"})
 
-    df.to_csv(output, index=False)
+def fetch_multiple(csv_file):
+    import pandas as pd
+    import os
+
+    subfolder = utils.make_valid_filename(os.path.basename(csv_file[:-4]))
+    print("Creating folder:", subfolder)
+    os.makedirs(f"data/{subfolder}/", exist_ok=True)
+
+    df = pd.read_csv(csv_file)
+
+    df_subset = zip(df["id"], df["display_name"], df["works_count"])
+
+    for host_venue_id, display_name, works_count in df_subset:
+        title = f"{host_venue_id}"
+        file_name = f"data/{subfolder}/{title}.csv"
+
+        print(f"Fetching {works_count} works for {display_name} ({host_venue_id})...")
+        fetch_single(host_venue_id, works_count, file_name)
 
 
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="")
     parser.add_argument(
         "host_venue",
-        help="The host venue ID to filter by",
+        help="The host venue ID to filter by or a csv file containing the column 'id', 'display_name', and 'works_count'",
     )
     parser.add_argument(
         "--fetch_limit",
@@ -58,4 +83,7 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    main(args.host_venue, args.fetch_limit, args.output)
+    if args.host_venue.endswith(".csv"):
+        fetch_multiple(args.host_venue)
+    else:
+        fetch_single(args.host_venue, args.fetch_limit, args.output)
