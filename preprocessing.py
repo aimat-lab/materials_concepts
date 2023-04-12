e0c196a148cc803c19329943426943b4c2f5a16f
thomasmarwitz <thomasmarwitz3@gmail.com>
2023-04-12 15:29:16 +0200

    Initial functions to preprocess abstracts.
    Cleaning involves:
    - filter out non-ascii
    - converting tabs to whitespaces
    - removing multiple whitespaces
    - removing enumerations in the text (roman and i's like)
    
    Authors are removed.
    Words 'abstract' and 'acknowledgements'.
    Introduction is removed, indicated by word 'introduction', usually followed by beginning of paper.
    
    Function included to extract and parse keywords (sometimes included at the end of abstract).

---
+++ preprocessing.py
@@ -0,0 +1,98 @@
+import re
+
+
+def replace_equation_symbols(text):
+    symbols = ["⩾", "⩽", "×", ">", "<", "=", "±", "%", "〈", "〉", "/", "∼"]
+    for symbol in symbols:
+        text = text.replace(symbol, " ")
+    return text
+
+
+def filter_out_ascii(text):
+    return "".join(char for char in text if ord(char) < 128)
+
+
+def remove_multiple_whitespaces(text):
+    text = re.sub(r"\s+", " ", text)
+    return text
+
+
+def remove_parenthesis(text):
+    text = re.sub(r"[\(\)]", "", text)
+    return text
+
+
+def remove_text_enumerations(text):  # RISKY: Dont remove element numbers
+    # with roman numerals
+    text = re.sub(r"\s+(i|ii|iii|iv|v)[\.\)]", "", text)
+    # with a, b, c, d
+    text = re.sub(r"\s+[a-e][\.\)]", "", text)
+
+    return text
+
+
+def clean_text(text):
+    text = filter_out_ascii(text)
+    # text = re.sub(" +", " ", text) ===> TEST
+    text = re.sub(r"\t+", " ", text)
+    text = remove_multiple_whitespaces(text)
+    # text = remove_parenthesis(text) ===> TEST
+    text = remove_text_enumerations(text)
+    text = text.strip()
+
+    return text
+
+
+def remove_authors(text):
+    text = re.sub(r"authors.*abstract", "", text, flags=re.IGNORECASE)
+    return text
+
+
+def remove_acknowledgements(text):
+    text = re.sub(r"acknowledgements", "", text, flags=re.IGNORECASE)
+    return text
+
+
+def remove_word(text, word):
+    text = re.sub(word, "", text, flags=re.IGNORECASE)
+    return text
+
+
+def parse_keywords(keywords):
+    comma_count = keywords.count(",")
+    semicolon_count = keywords.count(";")
+    delimiter = "," if comma_count > semicolon_count else ";"
+
+    keywords = keywords.split(delimiter)
+
+    TO_REMOVE = ".;,:"  # often a dot was found at the end of the last keyword
+    stripped_keywords = [keyword.strip(TO_REMOVE) for keyword in keywords]
+    return stripped_keywords
+
+
+def split_keywords(text):
+    text = re.split("keywords", text, flags=re.IGNORECASE)
+    return text
+
+
+def remove_intro(text):
+    text = re.sub(r"i(\s*)ntroduction.*", "", text, flags=re.IGNORECASE)
+    return text
+
+
+def clean_abstract(text):
+    # It's easier to catch elements if capitalized
+    text = clean_text(text)
+    text = remove_authors(text)
+    text = remove_word(text, "abstract")
+    text = remove_word(text, "acknowledgements")
+    text = remove_intro(text)
+    return text.strip()
+
+
+# FLAG OR REMOVE
+def language_cleaning(text):
+    "resumo"
+    "auteurs"
+    "autoren"
+    # wenn enthalten => manuell cleanen
