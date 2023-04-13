48b88d678d474931155e774e31afde5c531822d9
thomasmarwitz <thomasmarwitz3@gmail.com>
2023-04-13 13:37:14 +0200

    Test case for Chris Konop's rules

---
+++ utils/test_chem_tokenizer.py
@@ -0,0 +1,24 @@
+import unittest
+from chem_tokenizer import get_elements
+
+
+class TestChemTokenizer(unittest.TestCase):
+    def test_rules(self):
+        """Testing Chris Konop's rules"""
+        tests = (
+            ("Fe3 O", "Fe3O"),
+            ("Zr5 Al", "Zr5Al"),
+            ("Mn 4+", "Mn4+"),
+            ("Na 2", "Na2"),
+            ("Fe3O 4", "Fe3O4"),
+            ("Mn 0.75", "Mn0.75"),
+            ("Zn3 PO", "Zn3PO"),
+        )
+
+        for input, expected in tests:
+            with self.subTest(msg=f"Testing {input} -> {expected}"):
+                self.assertEqual(get_elements(input), {expected})
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
