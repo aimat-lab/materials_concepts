feef3508144472ca5f41a6f5fd98d8934408afc8
thomasmarwitz <thomasmarwitz3@gmail.com>
2023-05-16 14:54:12 +0200

    Local testing with LLaMa Tokenizer

---
+++ preparation/hpc/local_tokenizing.py
@@ -0,0 +1,43 @@
+from transformers import (
+    LlamaTokenizer,
+    DataCollatorForLanguageModeling,
+)
+import pandas as pd
+
+from torch.utils.data import Dataset, DataLoader
+
+tokenizer = LlamaTokenizer.from_pretrained("./HF/", return_tensors="pt")
+tokenizer.pad_token_id = 0  # unk. we want this to be different from the eos token
+
+CUTOFF_LENGTH = 2048
+
+
+class ConceptDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        row = self.data.iloc[idx]
+
+        text = row["abstract"] + " ==> " + str(row["tags"].split(","))
+
+        text_encodings = tokenizer(
+            text,
+            return_tensors="pt",
+        )
+
+        return {
+            "input_ids": text_encodings["input_ids"].flatten(),
+            "attention_mask": text_encodings["attention_mask"].flatten(),
+        }
+
+
+print("Loading dataset...")
+train_dataset = ConceptDataset(pd.read_csv("train.csv"))
+data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
+dataloader = DataLoader(
+    train_dataset, batch_size=4, shuffle=True, collate_fn=data_collator
+)
