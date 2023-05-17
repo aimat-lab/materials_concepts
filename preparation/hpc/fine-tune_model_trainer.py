1f722eeed745fcfe799c9ff17abb035086c68e33
thomasmarwitz <thomasmarwitz3@gmail.com>
2023-05-17 12:46:08 +0200

    Try out different settings, enable add_special_tokens, limit token padding for faster training

--- preparation/hpc/fine-tune_model_trainer.py
+++ preparation/hpc/fine-tune_model_trainer.py
@@ -31,7 +31,10 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 print("Loading tokenizer...")
 tokenizer = LlamaTokenizer.from_pretrained("./HF/", return_tensors="pt")
-tokenizer.pad_token_id = 0  # unk. we want this to be different from the eos token
+tokenizer.pad_token_id = 0
+tokenizer.bos_token_id = 1
+tokenizer.eos_token_id = 2
+
 
 print("Loading model...")
 model = LlamaForCausalLM.from_pretrained(
@@ -59,9 +62,10 @@ class ConceptDataset(Dataset):
         # Tokenize text and tags separately
         text_encodings = tokenizer(
             text,
+            add_special_tokens=True,
             return_tensors="pt",
             padding="max_length",
-            max_length=2048,
+            max_length=1024,
         )
 
         return {
@@ -75,14 +79,12 @@ print("Loading dataset...")
 dataset = ConceptDataset(pd.read_csv("train.csv"))
 
 dataset_size = len(dataset)
-train_size = int(0.9 * dataset_size)  # Let's say we want 80% of the data for training
+train_size = int(0.9 * dataset_size)
 test_size = dataset_size - train_size
 
 train_dataset, val_dataset = random_split(dataset, [train_size, test_size])
 
-data_collator = DataCollatorForLanguageModeling(
-    tokenizer, mlm=False, pad_to_multiple_of=8
-)
+data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
 
 
 ## Setup LORA
@@ -92,7 +94,7 @@ config = LoraConfig(
     r=16,  # attention heads
     lora_alpha=32,  # alpha scaling
     target_modules=["q_proj", "v_proj"],
-    lora_dropout=0.05,
+    lora_dropout=0.01,
     inference_mode=False,
 )
 
@@ -105,13 +107,13 @@ training_args = TrainingArguments(
     output_dir="./results",
     overwrite_output_dir=True,
     num_train_epochs=2,
-    weight_decay=0.01,
+    weight_decay=0.005,
     per_device_train_batch_size=1,  # reduce the batch size
     logging_dir="./logs",
     logging_steps=1,
     logging_strategy="steps",
     optim="adamw_torch",
-    learning_rate=0.0005,
+    learning_rate=1e-4,
     evaluation_strategy="epoch",
 )
 
