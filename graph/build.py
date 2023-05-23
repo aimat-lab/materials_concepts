5c93aa345010d74b5ef99f19ac39207c31509a12
thomasmarwitz <thomasmarwitz3@gmail.com>
2023-05-24 00:01:25 +0200

    Extract concepts, filtering, building edge list

--- graph/build.py
+++ graph/build.py
@@ -1,20 +1,164 @@
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
+from collections import Counter
+from ast import literal_eval
+from nltk import pos_tag
 
-# build lookup dict: concept -> id
+CONCEPTS_COLNAME = "rake_concepts"
+INPUT_FILE = "data/materials-science.rake.works.csv"
+ORIGIN_DAY = pd.to_datetime("1970-01-01")
+
+
+def str_to_set(str):
+    if isinstance(str, float):
+        return set()
+    if str == "":
+        return set()
+    return set(str.split(","))
+
+
+parse_func = {"rake_concepts": str_to_set, "llama_concepts": literal_eval}
+
+
+class OccuranceFilter:
+    def __init__(self, min_occurance=None, max_occurance=None):
+        self.min_occurance = min_occurance
+        self.max_occurance = max_occurance
+
+    def __call__(self, concepts):
+        print(
+            f"Applying occurance filter: min={self.min_occurance}, max={self.max_occurance}"
+        )
+        concepts = {
+            concept: n
+            for concept, n in tqdm(concepts.items())
+            if (self.min_occurance is None or n >= self.min_occurance)
+            and (self.max_occurance is None or n <= self.max_occurance)
+        }
+
+        return concepts
+
+
+class MinLenFilter:
+    def __init__(self, min_len):
+        self.min_len = min_len
+
+    def __call__(self, concepts):
+        print(f"Applying min len filter: min={self.min_len}")
+        concepts = {
+            concept: n
+            for concept, n in tqdm(concepts.items())
+            if len(concept) >= self.min_len
+        }
+
+        return concepts
+
+
+class RakeFilter:
+    REMOVE_IF_LEN_2 = [
+        "important",
+        "also",
+        "due",
+        "thoroughly",
+        "using",
+        "showed",
+        "much",
+        "times",
+    ]
+
+    REMOVE_ALWAYS = [
+        "results",
+        "result",
+        "recent",
+        "recently",
+        "present",
+        "presented",
+        "presents",
+        "work",
+        "works",
+        "experiment",
+        "experiments",
+        "experimental",
+        "paper",
+        "papers",
+    ]
+
+    def __call__(self, concepts):
+        print("Applying rake filter")
+
+        concepts = {
+            concept: n
+            for concept, n in tqdm(concepts.items())
+            if all(c not in self.REMOVE_IF_LEN_2 for c in concept.split(" "))
+            and concept.count(" ") <= 1
+        }
+
+        concepts = {
+            concept: n
+            for concept, n in tqdm(concepts.items())
+            if all(c not in self.REMOVE_ALWAYS for c in concept.split(" "))
+        }
+
+        concepts = {
+            concept: n
+            for concept, n in tqdm(concepts.items())
+            if RakeFilter.is_meaningful(concept)
+        }
+
+        return concepts
+
+    @staticmethod
+    def is_meaningful(phrase):
+        pos_tags = pos_tag(phrase.split(" "))
+        pos_tags = [tag[1] for tag in pos_tags]
+
+        # Remove phrases with adverbs, prepositions, conjunctions, or determiners in them if they are <= 2 words
+        if (
+            any(tag in {"RB", "IN", "CC", "DT", "JJ", "JJR"} for tag in pos_tags)
+            and len(phrase.split(" ")) <= 2
+        ):
+            return False
+
+        return True
+
+
+df = pd.read_csv(INPUT_FILE)
+df[CONCEPTS_COLNAME] = df[CONCEPTS_COLNAME].apply(parse_func[CONCEPTS_COLNAME])
+df.elements = df.elements.apply(str_to_set)
+
+print("Building concept list")
+concepts = [concept for s in tqdm(df[CONCEPTS_COLNAME]) for concept in s]
+elements = [element for s in tqdm(df.elements) for element in s]
+
+concepts = Counter(concepts)
+elements = Counter(elements)
+
+c_filters = [OccuranceFilter(min_occurance=3), MinLenFilter(7), RakeFilter()]
+e_filters = [OccuranceFilter(min_occurance=3)]
+
+print(f"Number of concepts: {len(concepts)}")
+for filter in c_filters:
+    concepts = filter(concepts)
+print(f"Number of concepts: {len(concepts)}")
+
+print(f"Number of elements: {len(elements)}")
+for filter in e_filters:
+    elements = filter(elements)
+print(f"Number of elements: {len(elements)}")
+
+concept_list = list(concepts.keys()) + list(elements.keys())
+
+# transform concepts into numbers
 lookup = {}
-with open("data/lists/all_concepts_filtered.txt") as f:
-    for index, line in enumerate(f.readlines()):
-        concept = line.strip()
-        lookup[concept] = index
+for index, concept in enumerate(sorted(concept_list)):
+    concept = concept.strip()
+    lookup[concept] = index
 
-df = pd.read_csv("data/materials-science.rake.works.csv")
-df = df.dropna(subset=["rake_concepts"])  # drop rows with no concepts
 
-origin_day = pd.to_datetime("1970-01-01")
+# encode publication date as days since origin
 df["pub_date_days"] = pd.to_datetime(df.publication_date).apply(
-    lambda ts: (ts - origin_day).days
+    lambda ts: (ts - ORIGIN_DAY).days
 )
 
 
@@ -30,10 +174,11 @@ def get_pairs(items):
     return pairs
 
 
+print("Building edge list")
 all_edges = []
-for concept_list, pub_date in tqdm(list(zip(df.rake_concepts, df.pub_date_days))):
+for concept_list, pub_date in tqdm(list(zip(df[CONCEPTS_COLNAME], df.pub_date_days))):
     concept_ids = {
-        lookup[c] for c in concept_list.split(",") if lookup.get(c) is not None
+        lookup[c] for c in concept_list if lookup.get(c) is not None
     }  # set comprehension because rake doesn't filter out duplicates
 
     for v1, v2 in get_pairs(concept_ids):
