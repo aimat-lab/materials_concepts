faebfc185d669ffde161a42be3df7a4f476bcb4c
thomasmarwitz <thomasmarwitz3@gmail.com>
2023-06-21 11:56:47 +0200

    Improve workflow for generating graph, add filter mechanism for concepts.
    For example, concepts with double elements (PP, HH, VII) are now filtered.
    Add documentation in README

--- graph/build.py
+++ graph/build.py
@@ -4,9 +4,9 @@ from tqdm import tqdm
 from collections import Counter
 from ast import literal_eval
 from nltk import pos_tag
+import re
+from fire import Fire
 
-CONCEPTS_COLNAME = "rake_concepts"
-INPUT_FILE = "data/materials-science.rake.works.csv"
 ORIGIN_DAY = pd.to_datetime("1970-01-01")
 
 
@@ -18,38 +18,20 @@ def str_to_set(str):
     return set(str.split(","))
 
 
-parse_func = {"rake_concepts": str_to_set, "llama_concepts": literal_eval}
-
-
-class OccuranceFilter:
-    def __init__(self, min_occurance=None, max_occurance=None):
-        self.min_occurance = min_occurance
-        self.max_occurance = max_occurance
+class OccurenceFilter:
+    def __init__(self, min_occurence=None, max_occurence=None):
+        self.min_occurence = min_occurence
+        self.max_occurence = max_occurence
 
     def __call__(self, concepts):
         print(
-            f"Applying occurance filter: min={self.min_occurance}, max={self.max_occurance}"
+            f"Applying occurence filter: min={self.min_occurence}, max={self.max_occurence}"
         )
         concepts = {
             concept: n
             for concept, n in tqdm(concepts.items())
-            if (self.min_occurance is None or n >= self.min_occurance)
-            and (self.max_occurance is None or n <= self.max_occurance)
-        }
-
-        return concepts
-
-
-class MinLenFilter:
-    def __init__(self, min_len):
-        self.min_len = min_len
-
-    def __call__(self, concepts):
-        print(f"Applying min len filter: min={self.min_len}")
-        concepts = {
-            concept: n
-            for concept, n in tqdm(concepts.items())
-            if len(concept) >= self.min_len
+            if (self.min_occurence is None or n >= self.min_occurence)
+            and (self.max_occurence is None or n <= self.max_occurence)
         }
 
         return concepts
@@ -123,73 +105,183 @@ class RakeFilter:
         return True
 
 
-df = pd.read_csv(INPUT_FILE)
-df[CONCEPTS_COLNAME] = df[CONCEPTS_COLNAME].apply(parse_func[CONCEPTS_COLNAME])
-df.elements = df.elements.apply(str_to_set)
+class LlamaFilter:
+    def __call__(self, concepts):
+        print("Applying LLaMa filter")
+        concepts = {
+            concept: n
+            for concept, n in tqdm(concepts.items())
+            if not re.findall(r"\b\d+\b", concept)  # no standalone numbers
+        }
 
-print("Building concept list")
-concepts = [concept for s in tqdm(df[CONCEPTS_COLNAME]) for concept in s]
-elements = [element for s in tqdm(df.elements) for element in s]
-
-concepts = Counter(concepts)
-elements = Counter(elements)
-
-c_filters = [OccuranceFilter(min_occurance=3), MinLenFilter(7), RakeFilter()]
-e_filters = [OccuranceFilter(min_occurance=3)]
-
-print(f"Number of concepts: {len(concepts)}")
-for filter in c_filters:
-    concepts = filter(concepts)
-print(f"Number of concepts: {len(concepts)}")
-
-print(f"Number of elements: {len(elements)}")
-for filter in e_filters:
-    elements = filter(elements)
-print(f"Number of elements: {len(elements)}")
-
-concept_list = list(concepts.keys()) + list(elements.keys())
-
-# transform concepts into numbers
-lookup = {}
-for index, concept in enumerate(sorted(concept_list)):
-    concept = concept.strip()
-    lookup[concept] = index
-
-# save lookup as csv
-lookup_df = pd.DataFrame.from_dict(lookup, orient="index", columns=["id"])
-lookup_df.to_csv("graph/lookup.csv")
-
-# encode publication date as days since origin
-df["pub_date_days"] = pd.to_datetime(df.publication_date).apply(
-    lambda ts: (ts - ORIGIN_DAY).days
-)
+        return concepts
 
 
-def get_pairs(items):
-    pairs = []
-    for i1 in items:
-        for i2 in items:
-            if i1 == i2:
-                # this ensures that we don't get to the diagonal line in the pairing matrix
-                # as order doesn't matter, this yields just half of the matrix (excluding the diagonal)
-                break
-            pairs.append((i1, i2))
-    return pairs
+class NonAsciiFilter:
+    def __call__(self, concepts):
+        print("Applying non-ascii filter")
+        concepts = {
+            concept: n
+            for concept, n in tqdm(concepts.items())
+            if all(ord(c) < 128 for c in concept)
+        }
+
+        return concepts
 
 
-print("Building edge list")
-all_edges = []
-for concept_list, pub_date in tqdm(list(zip(df[CONCEPTS_COLNAME], df.pub_date_days))):
-    concept_ids = {
-        lookup[c] for c in concept_list if lookup.get(c) is not None
-    }  # set comprehension because rake doesn't filter out duplicates
+class WordFilter:
+    def __init__(self, min_n=1, max_n=10):
+        self.min_n = min_n
+        self.max_n = max_n
 
-    for v1, v2 in get_pairs(concept_ids):
-        all_edges.append(np.array((v1, v2, pub_date)))
+    def __call__(self, concepts):
+        print(f"Applying word filter: min={self.min_n}, max={self.max_n}")
+        concepts = {
+            concept: n
+            for concept, n in tqdm(concepts.items())
+            if self.min_n <= len(concept.split(" ")) <= self.max_n
+        }
+
+        return concepts
 
 
-all_edges = np.array(all_edges)
-np.savez_compressed("graph/edges.npz", all_edges)
+class ElementFilter:
+    """Filters non-valid elements"""
 
-# load edges
-# edges = np.load("graph/edges.npz", allow_pickle=True)["arr_0"]
+    def __init__(self, min_amount_elements=2):
+        self.min_amount_elements = min_amount_elements
+
+    @staticmethod
+    def get_elements(string):
+        counts = {}
+        _buffer = ""
+        for c in string:
+            if c.isupper():  # new element
+                if _buffer != "":
+                    counts[_buffer] = counts.get(_buffer, 0) + 1
+
+                _buffer = c
+
+            elif c.isalpha():  # no number
+                _buffer += c
+
+        counts[_buffer] = counts.get(_buffer, 0) + 1
+
+        return counts
+
+    @staticmethod
+    def is_valid_element(elements: dict):
+        return all(
+            n <= 1 for n in elements.values()
+        )  # no duplicates in elements allowed
+
+    def __call__(self, elements):
+        print("Applying elements filter")
+        elements = {
+            element: n
+            for element, n in tqdm(elements.items())
+            if len(self.get_elements(element)) >= self.min_amount_elements
+            and self.is_valid_element(self.get_elements(element))
+        }
+
+        return elements
+
+
+E_FILTERS = [OccurenceFilter(min_occurence=3), ElementFilter(min_amount_elements=2)]
+
+settings = {
+    "rake_concepts": {
+        "parse_func": str_to_set,
+        "filters": [OccurenceFilter(min_occurence=3), RakeFilter()],
+    },
+    "llama_concepts": {
+        "parse_func": literal_eval,
+        "filters": [
+            OccurenceFilter(min_occurence=3),
+            LlamaFilter(),
+            NonAsciiFilter(),
+            WordFilter(min_n=3, max_n=10),
+        ],
+    },
+}
+
+
+def main(
+    input_file="data/materials-science.llama.works.csv",
+    colname="llama_concepts",
+):
+    df = pd.read_csv(input_file)
+    df[colname] = df[colname].apply(settings[colname]["parse_func"])
+    df.elements = df.elements.apply(str_to_set)
+
+    concepts = [concept for s in tqdm(df[colname]) for concept in s]
+    elements = [element for s in tqdm(df.elements) for element in s]
+
+    concepts = Counter(concepts)
+    elements = Counter(elements)
+
+    for filter in settings[colname]["filters"]:
+        concepts = filter(concepts)
+
+    for filter in E_FILTERS:
+        elements = filter(elements)
+
+    concept_list = list(concepts.keys()) + list(elements.keys())
+
+    # transform concepts into numbers
+    lookup = {}
+    lookup_list = []
+
+    for index, concept in enumerate(sorted(concept_list)):
+        concept = concept.strip()
+        lookup_list.append(
+            {
+                "id": index,
+                "concept": concept,
+                "count": concepts.get(concept, elements.get(concept, 0)),
+            }
+        )
+        lookup[concept] = index
+
+    # save lookup as csv
+    lookup_df = pd.DataFrame(lookup_list)
+    lookup_df.to_csv("graph/lookup.csv", index=False)
+
+    # encode publication date as days since origin
+    df["pub_date_days"] = pd.to_datetime(df.publication_date).apply(
+        lambda ts: (ts - ORIGIN_DAY).days
+    )
+
+    def get_pairs(items):
+        pairs = []
+        for i1 in items:
+            for i2 in items:
+                if i1 == i2:
+                    # this ensures that we don't get to the diagonal line in the pairing matrix
+                    # as order doesn't matter, this yields just half of the matrix (excluding the diagonal)
+                    break
+                pairs.append((i1, i2))
+        return pairs
+
+    print("Building edge list")
+    all_edges = []
+    for concept_list, pub_date in tqdm(list(zip(df[colname], df.pub_date_days))):
+        concept_ids = {
+            lookup[c] for c in concept_list if lookup.get(c) is not None
+        }  # set comprehension because rake/llms don't necessarily filter out duplicates
+
+        for v1, v2 in get_pairs(concept_ids):
+            all_edges.append(np.array((v1, v2, pub_date)))
+
+    all_edges = np.array(all_edges)
+
+    print(f"# edges: {len(all_edges): .0f}")
+
+    np.savez_compressed("graph/edges.npz", all_edges)
+
+    # load edges
+    # edges = np.load("graph/edges.npz", allow_pickle=True)["arr_0"]
+
+
+if __name__ == "__main__":
+    Fire(main)
