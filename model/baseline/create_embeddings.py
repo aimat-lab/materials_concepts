b1523840105892c034d63b18a7324cf0465477ee
thomasmarwitz <thomasmarwitz3@gmail.com>
2023-06-30 15:37:40 +0200

    Scriptify and jaccard_coeff

--- model/baseline/create_embeddings.py
+++ model/baseline/create_embeddings.py
@@ -1,6 +1,6 @@
 from tqdm import tqdm
 import numpy as np
-
+import fire
 import os
 import sys
 
@@ -16,6 +16,20 @@ def normalize_degrees(degrees):
     return degrees / np.max(degrees) if np.max(degrees) > 0 else degrees
 
 
+def get_jaccard_coefficient(
+    u: int, v: int, num_neighbors: np.ndarray, adjacency_matrix_squared: np.ndarray
+):
+    num_neighbors_intersection = adjacency_matrix_squared[u, v]
+    num_neighbors_union = (
+        num_neighbors[u] + num_neighbors[v] - num_neighbors_intersection
+    )
+    return (
+        num_neighbors_intersection / num_neighbors_union
+        if num_neighbors_union > 0
+        else None
+    )
+
+
 def retrieve_properties_for_vertex_pair(
     AA02,
     AA12,
@@ -23,38 +37,58 @@ def retrieve_properties_for_vertex_pair(
     all_degs0,
     all_degs1,
     all_degs2,
+    all_degs0_n,
+    all_degs1_n,
+    all_degs2_n,
     all_degs02,
     all_degs12,
     all_degs22,
     v1,
     v2,
+    include_jaccard=True,
 ):
     """
     Computes hand-crafted properties for one vertex in vlist
     """
     all_properties = []
 
-    all_properties.append(all_degs0[v1])  # 0
-    all_properties.append(all_degs0[v2])  # 1
-    all_properties.append(all_degs1[v1])  # 2
-    all_properties.append(all_degs1[v2])  # 3
-    all_properties.append(all_degs2[v1])  # 4
-    all_properties.append(all_degs2[v2])  # 5
+    all_properties.append(all_degs0_n[v1])  # 0
+    all_properties.append(all_degs0_n[v2])  # 1
+    all_properties.append(all_degs1_n[v1])  # 2
+    all_properties.append(all_degs1_n[v2])  # 3
+    all_properties.append(all_degs2_n[v1])  # 4
+    all_properties.append(all_degs2_n[v2])  # 5
     all_properties.append(all_degs02[v1])  # 6
     all_properties.append(all_degs02[v2])  # 7
     all_properties.append(all_degs12[v1])  # 8
     all_properties.append(all_degs12[v2])  # 9
     all_properties.append(all_degs22[v1])  # 10
     all_properties.append(all_degs22[v2])  # 11
-
     all_properties.append(AA02[v1, v2])  # 12
     all_properties.append(AA12[v1, v2])  # 13
     all_properties.append(AA22[v1, v2])  # 14
 
+    if include_jaccard:
+        all_properties.append(
+            get_jaccard_coefficient(
+                u=v1, v=v2, num_neighbors=all_degs0, adjacency_matrix_squared=AA02
+            )
+        )
+        all_properties.append(
+            get_jaccard_coefficient(
+                u=v1, v=v2, num_neighbors=all_degs1, adjacency_matrix_squared=AA12
+            )
+        )
+        all_properties.append(
+            get_jaccard_coefficient(
+                u=v1, v=v2, num_neighbors=all_degs2, adjacency_matrix_squared=AA22
+            )
+        )
+
     return np.array(all_properties).astype(np.float32)
 
 
-def compute_all_properties_of_list(all_sparse, vlist):
+def compute_all_properties_of_list(all_sparse, vlist, include_jaccard=True):
     """
     Computes hand-crafted properties for all vertices in vlist
 
@@ -76,9 +110,12 @@ def compute_all_properties_of_list(all_sparse, vlist):
 
     print("done")
 
-    all_degs0 = normalize_degrees(get_degrees(all_sparse[0]))
-    all_degs1 = normalize_degrees(get_degrees(all_sparse[1]))
-    all_degs2 = normalize_degrees(get_degrees(all_sparse[2]))
+    all_degs0 = get_degrees(all_sparse[0])
+    all_degs0_n = normalize_degrees(all_degs0)
+    all_degs1 = get_degrees(all_sparse[1])
+    all_degs1_n = normalize_degrees(all_degs1)
+    all_degs2 = get_degrees(all_sparse[2])
+    all_degs2_n = normalize_degrees(all_degs2)
 
     all_degs02 = normalize_degrees(get_degrees(AA02[0]))
     all_degs12 = normalize_degrees(get_degrees(AA12[1]))
@@ -96,11 +133,15 @@ def compute_all_properties_of_list(all_sparse, vlist):
             all_degs0,
             all_degs1,
             all_degs2,
+            all_degs0_n,
+            all_degs1_n,
+            all_degs2_n,
             all_degs02,
             all_degs12,
             all_degs22,
             vlist[ii][0],
             vlist[ii][1],
+            include_jaccard=include_jaccard,
         )
 
         all_properties.append(vals)
@@ -108,27 +149,37 @@ def compute_all_properties_of_list(all_sparse, vlist):
     return all_properties
 
 
-def calculate_embeddings(X_train, X_test):
+def calculate_embeddings(X_train, X_test, include_jaccard=True):
     graph = Graph("graph/edges.pkl")
 
     matrices = graph.get_adj_matrices([2010, 2013, 2016])
     embeddings = compute_all_properties_of_list(
-        matrices, np.concatenate([X_train, X_test])
+        matrices, np.concatenate([X_train, X_test]), include_jaccard=include_jaccard
     )
 
     return embeddings[: len(X_train)], embeddings[len(X_train) :]
 
 
-if __name__ == "__main__":
+def main(
+    data_path="model/data.pkl",
+    output_path="model/baseline/embeddings.pkl",
+    include_jaccard=True,
+):
     import pickle
 
     print("Loading data...")
-    with open("model/data.pkl", "rb") as f:
+    with open(data_path, "rb") as f:
         data = pickle.load(f)
 
     print("Calculating embeddings...")
-    X_train, X_test = calculate_embeddings(data["X_train"], data["X_test"])
+    X_train, X_test = calculate_embeddings(
+        data["X_train"], data["X_test"], include_jaccard=include_jaccard
+    )
 
     print("Saving embeddings...")
-    with open("model/baseline/embeddings.pkl", "wb") as f:
+    with open(output_path, "wb") as f:
         pickle.dump({"X_train": X_train, "X_test": X_test}, f)
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
