cf39a554d92fd942969def3622800ab63ac7dab5
thomasmarwitz <thomasmarwitz3@gmail.com>
2023-07-24 12:16:38 +0200

    Moved to own dir

--- api/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-
-app = FastAPI()
-
-
-@app.get("/search")
-def search(query: str, semantic_search: bool = False):
-    return {"message": f"You searched for {query}"}
