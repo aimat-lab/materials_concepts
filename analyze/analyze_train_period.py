5389a0d1f139b3323667f087baf8267c5adf14df
thomasmarwitz <thomasmarwitz3@gmail.com>
2023-09-18 12:14:26 +0200

    Graph analysis

---
+++ analyze/analyze_train_period.py
@@ -0,0 +1,46 @@
+import numpy as np
+from utils import load
+from collections import Counter
+from graph import Graph
+from tqdm import tqdm
+import bfs
+import sys
+import fire
+
+
+def main(
+    N=10000,
+    data_path="data/model/data.M.pkl",
+    graph_path="data/graph/edges.M.pkl",
+):
+    print("Loading data")
+    data = load(data_path)
+
+    pos_index = np.where(data["y_train"] == 1)
+    pos_edges = data["X_train"][pos_index]
+
+    c = Counter()
+
+    for edge in pos_edges:
+        c.update(edge)
+
+    print("Loading graph")
+    g = Graph.from_path(graph_path).get_nx_graph(data["year_train"])
+
+    depthCounter = Counter()
+
+    # sample N edges
+    sample = np.random.choice(len(pos_edges), N, replace=False)
+    for i, (u, v) in tqdm(enumerate(pos_edges[sample]), total=N):
+        distance = bfs.distance(g, u, v)
+        depthCounter.update([distance])
+
+        if (i + 1) % 1000 == 0:
+            print(f"Depth Counter: {depthCounter}")
+
+    print("Finished")
+    print(f"Depth Counter: {depthCounter}")
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
